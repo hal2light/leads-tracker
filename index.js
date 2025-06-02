c8f277e27b17f22a531c41c36c0ccd152430eb88
hal2light <halitberken09@gmail.com>
2025-06-02 14:01:43 +0300

    fixed the code

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
+let myLeads = []
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -32,16 +24,24 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
-
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
-
-inputBtn.addEventListener("click", function() {
+function saveInput(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
-})
+}
+function saveTab(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+}
+function deleteAll(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads) 
+}
+inputBtn.addEventListener("click", saveInput)
+tabBtn.addEventListener("click", saveTab)
+deleteBtn.addEventListener("dblclick",deleteAll)
